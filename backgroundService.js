c79d7d484ea6d31bfb6b31c585595e186ac2f7bf
Michael A. Pedersen <cra7y.com@gmail.com>
2025-04-15 11:40:18 +0200

    Fixed backgsround service and  added start command

--- backgroundService.js
+++ backgroundService.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import { playAudio } from "openai/helpers/audio";
 
 const openai = new OpenAI({
-  apikey: process.env.OPENAI_TOKEN,
+  apiKey: "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",
 });
 
 const io = new Server({
@@ -13,7 +13,7 @@ const io = new Server({
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected"); 
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
